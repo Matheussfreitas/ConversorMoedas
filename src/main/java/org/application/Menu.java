d4aaa2e6aefdf33872037e7e61c914132f30fc19
Matheus Freitas <mgsoutodelira@gmail.com>
2024-06-09 10:27:23 -0300

    refactore code and news updates

--- src/main/java/org/application/Menu.java
+++ src/main/java/org/application/Menu.java
@@ -9,80 +9,81 @@ public class Menu {
     private final String URI_BASE = "https://v6.exchangerate-api.com/v6/b147c45971c45b829e4c659b/pair/";
 
     public void exibeMenu() {
+        String menuMoedas = """
+                1) Dólar(USD) 
+                2) Euro(EUR)
+                3) Peso Argentino(ARS)
+                * OBS: Cotação em relação ao valor do Real Brasileiro *
+                """;
         String menu = """
-                1) Dólar -> Real
-                2) Real -> Dólar
-                3) Euro -> Real Brasileiro
-                4) Real Brasileiro -> Euro
-                5) Peso Argentino -> Real Brasileiro
-                6) Real Brasileiro -> Peso Argentino
+                1) Dólar(USD) -> Real Brasileiro(BRL)
+                2) Real Brasileiro(BRL) -> Dólar(USD)
+                3) Euro(EUR) -> Real Brasileiro(BRL)
+                4) Real Brasileiro(BRL) -> Euro(EUR)
+                5) Peso Argentino(ARS) -> Real Brasileiro(BRL)
+                6) Real Brasileiro(BRL) -> Peso Argentino(ARS)
+                7) Verificar Cotação Atual
                 * Para sair digite qualquer outro número *
                 """;
         System.out.println(menu);
 
-        String endereco;
-        String json;
-        Moedas converteJson;
-        double taxaConversao;
-        double resultado;
-
         System.out.println("Informe a operação que deseja realizar: ");
         var operacao = scanner.nextInt();
+        
+        if (operacao < 1 || operacao > 7) {
+            System.out.println("Saindo...");
+            return;
+        } if (operacao == 7) {
+                System.out.println("\n" + menuMoedas);
+                System.out.println("Selecione a moeda para obter a cotação: ");
+                int operacaoCotacao = scanner.nextInt();
+            try {
+                verificarCotacao(operacaoCotacao);
+                return;
+            } catch (Exception e) {
+                System.out.println("Erro ao processar a informação: " + e.getMessage());
+            }
+        }
+
+        
         System.out.println("Informe o valor que deseja converter: ");
         var valor = scanner.nextDouble();
-        
-        switch (operacao) {
-            case 1:
-                endereco = URI_BASE + "USD/BRL";
-                json = consumoApi.obterDados(endereco);
-                converteJson = converteDados.obterDados(json, Moedas.class);
-                taxaConversao = converteJson.conversao();
-                resultado = valor * taxaConversao;
-                System.out.println("O resultado da conversão de " + converteJson.moeda() + " para " + converteJson.moedaConvertida() + " é de: " + resultado);
-                break;
-            case 2:
-                endereco = URI_BASE + "BRL/USD";
-                json = consumoApi.obterDados(endereco);
-                converteJson = converteDados.obterDados(json, Moedas.class);
-                taxaConversao = converteJson.conversao();
-                resultado = valor * taxaConversao;
-                System.out.println(resultado);
-                break;
-            case 3:
-                endereco = URI_BASE + "EUR/BRL";
-                json = consumoApi.obterDados(endereco);
-                converteJson = converteDados.obterDados(json, Moedas.class);
-                taxaConversao = converteJson.conversao();
-                resultado = valor * taxaConversao;
-                System.out.println(resultado);
-                break;
-            case 4:
-                endereco = URI_BASE + "BRL/EUR";
-                json = consumoApi.obterDados(endereco);
-                converteJson = converteDados.obterDados(json, Moedas.class);
-                taxaConversao = converteJson.conversao();
-                resultado = valor * taxaConversao;
-                System.out.println(resultado);
-                break;
-            case 5: 
-                endereco = URI_BASE + "ARS/BRL";
-                json = consumoApi.obterDados(endereco);
-                converteJson = converteDados.obterDados(json, Moedas.class);
-                taxaConversao = converteJson.conversao();
-                resultado = valor * taxaConversao;
-                System.out.println(resultado);
-                break;  
-            case 6:
-                endereco = URI_BASE + "BRL/ARS";
-                json = consumoApi.obterDados(endereco);
-                converteJson = converteDados.obterDados(json, Moedas.class);
-                taxaConversao = converteJson.conversao();
-                resultado = valor * taxaConversao;
-                System.out.println(resultado);
-                break;
-            default:
-                System.out.println("Saindo");
-                break;
-        }
+        scanner.close();
+
+        try {
+            selecionarConversao(operacao, valor);
+        } catch (Exception e) {
+            System.out.println("Erro ao processar a informação: " + e.getMessage());
+        };
     }    
+    
+    public void selecionarConversao(int operacao, double valor) {
+        String[] moedas = {
+            "USD/BRL",
+            "BRL/USD",
+            "EUR/BRL",
+            "BRL/EUR",
+            "ARS/BRL",
+            "BRL/ARS"
+        };
+        String endereco = URI_BASE + moedas[operacao - 1];
+        String json = consumoApi.obterDados(endereco);
+        Moedas converteJson = converteDados.obterDados(json, Moedas.class);
+        double taxaConversao = converteJson.conversao();
+        double resultado = valor * taxaConversao;
+        System.out.println("O resultado da conversão de " + converteJson.moeda() + " para " + converteJson.moedaConvertida() + " é de: " + resultado);
+    }
+
+    public void verificarCotacao (int operacaoCotacao) {
+        String[] moedas = {
+            "USD/BRL",
+            "EUR/BRL",
+            "ARS/BRL"
+        };
+        String endereco = URI_BASE + moedas[operacaoCotacao - 1];
+        String json = consumoApi.obterDados(endereco);
+        Moedas converteJson = converteDados.obterDados(json, Moedas.class);
+        double taxaConversao = converteJson.conversao();
+        System.out.println("A cotação atual é de: " + converteJson.moeda() + ": " + taxaConversao); 
+    }
 }
